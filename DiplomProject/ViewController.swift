1c95bfacbcba0cb9475ebdda0e4edf0b9e5385d6
Илья Сидорик <iliasidorik@MacBook-Pro-Ila-2.local>
2025-02-15 16:40:05 +0300

    create empty project

---
+++ DiplomProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiplomProject
+//
+//  Created by Илья Сидорик on 15.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
